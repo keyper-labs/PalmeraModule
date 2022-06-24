e705c493156afe8bc12dfc33e6012bbd7e2f6f48
Jose Ramirez <jose.ramirez@seba.swiss>
2022-06-24 14:59:15 +0200

    add forge lib deps as submodule

---
+++ script/DeployModule.t.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.13;
+import "forge-std/Script.sol";
+import "src/KeyperModule.sol";
+
+contract DeployModule is Script{
+    function run() public {
+        vm.startBroadcast();
+        KeyperModule keyperModule = new KeyperModule();
+        vm.stopBroadcast();
+    }
+}
---
+++ test/deploy.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../script/DeployModule.t.sol";
+
+contract TestDeploy is Test {
+    DeployModule deploy;
+
+    function setUp() public {
+        deploy = new DeployModule();
+    }
+
+    function testDeploy() public {
+        deploy.run();
+    }
+}
+
