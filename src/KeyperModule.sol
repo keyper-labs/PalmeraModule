09d12a8a7fbce17a44ca46d0e2e0f5a9b821ba37
Jose Ramirez <jose.ramirez@seba.swiss>
2022-07-05 20:49:42 +0200

    wip: signature verification module

--- src/KeyperModule.sol
+++ src/KeyperModule.sol
@@ -2,7 +2,11 @@
 pragma solidity ^0.8.0;
 
 import {Enum} from "@safe-contracts/common/Enum.sol";
+import {SignatureDecoder} from "@safe-contracts/common/SignatureDecoder.sol";
+import {ISignatureValidator} from "@safe-contracts/interfaces/ISignatureValidator.sol";
+import {ISignatureValidatorConstants} from "@safe-contracts/interfaces/ISignatureValidator.sol";
 import {console} from "forge-std/console.sol";
+import {GnosisSafeMath} from "@safe-contracts/external/GnosisSafeMath.sol";
 
 interface GnosisSafe {
     /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
@@ -46,14 +50,32 @@ interface GnosisSafe {
         external
         payable
         returns (bool success);
+
+    /// @dev Returns array of owners.
+    /// @return Array of Safe owners.
+    function getOwners() external view returns (address[] memory);
+
+    function getThreshold() external view returns (uint256);
 }
 
 // TODO modifiers for auth calling the diff functions
 // TODO define how secure this setup should be: Calls only from Admin? Calls from safe contract (with multisig rule)
 // TODO update signers set
-contract KeyperModule {
+contract KeyperModule is SignatureDecoder, ISignatureValidatorConstants {
     string public constant NAME = "Keyper Module";
     string public constant VERSION = "0.1.0";
+
+    // keccak256(
+    //     "EIP712Domain(uint256 chainId,address verifyingContract)"
+    // );
+    bytes32 private constant DOMAIN_SEPARATOR_TYPEHASH = 0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218;
+
+    // keccak256(
+    //     "KeyperTx(address org,address safe,address to,uint256 value,bytes data,uint8 operation,uint256 nonce)"
+    // );
+    bytes32 private constant KEYPER_TX_TYPEHASH = 0xbb667b7bf67815e546e48fb8d0e6af5c31fe53b9967ed45225c9d55be21652da;
+    using GnosisSafeMath for uint256;
+
 
     // Orgs -> Groups
     mapping(address => mapping(address => Group)) public groups;
@@ -62,6 +84,9 @@ contract KeyperModule {
 
     // Orgs info
     mapping(address => Group) public orgs;
+
+    uint256 public nonce;
+    address internal constant SENTINEL_OWNERS = address(0x1);
 
     // Errors
     error OrgNotRegistered();
@@ -199,41 +224,167 @@ contract KeyperModule {
         return group.childs[_child];
     }
 
-    // TODO: Check if we need to implement this function for the demo
-    // function associateChild(address _org, address _group, address _child) private
-    function execKeeperTransaction(
-        address _org,
+    // TODO remove org param 
+    function execTransactionOnBehalf(
+        address org,
         address safe,
         address to,
         uint256 value,
         bytes calldata data,
-        TransactionHelper calldata txHelper,
-        address payable refundReceiver,
+        Enum.Operation operation,
         bytes memory signatures
     )
         external
         payable
         returns (bool success)
     {
-        // TODO Remove this dummy logic
-        // if (orgs[_org].safe == address(0)) revert OrgNotRegistered();
-        // check msg.sender is the admin of the _org
-        if (msg.sender != _org) revert NotAuthorized();
+        // Check if msg.sender is an org
+        (, address admin, , address parent) = getGroupInfo(org, safe);
+        // Verify msg.sender is admin or parent
+        if (msg.sender != admin || msg.sender != parent) revert NotAuthorized();
+        GnosisSafe gnosisSafe = GnosisSafe(msg.sender);
+        bytes32 txHash;
+        // Use scope here to limit variable lifetime and prevent `stack too deep` errors
+        {
+            bytes memory txHashData =
+                encodeTransactionData(
+                    // Transaction info
+                    to,
+                    value,
+                    data,
+                    operation,
+                    // Signature info
+                    nonce
+                );
+            // Increase nonce and execute transaction.
+            nonce++;
+            txHash = keccak256(txHashData);
+            checkNSignatures(txHash, txHashData, signatures, gnosisSafe);
+            return true;
+        }
+    }
 
-        // Init gnosis safe contract
-        GnosisSafe gnosisSafe = GnosisSafe(safe);
-        return
-            gnosisSafe.execTransaction(
-                to,
-                value,
-                data,
-                txHelper.operation,
-                txHelper.safeTxGas,
-                txHelper.baseGas,
-                txHelper.gasPrice,
-                txHelper.gasToken,
-                refundReceiver,
-                signatures
+    function encodeTransactionData(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation,
+        uint256 _nonce
+    ) public view returns (bytes memory) {
+        bytes32 keyperTxHash =
+            keccak256(
+                abi.encode(
+                    KEYPER_TX_TYPEHASH,
+                    to,
+                    value,
+                    keccak256(data),
+                    operation,
+                    _nonce
+                )
             );
+        return abi.encodePacked(bytes1(0x19), bytes1(0x01), domainSeparator(), keyperTxHash);
+    }
+
+    function domainSeparator() public view returns (bytes32) {
+        return keccak256(abi.encode(DOMAIN_SEPARATOR_TYPEHASH, getChainId(), this));
+    }
+
+    /// @dev Returns the chain id used by this contract.
+    function getChainId() public view returns (uint256) {
+        uint256 id;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+
+    /**
+     * @dev Checks whether the signature provided is valid for the provided data, hash. Will revert otherwise.
+     * @param dataHash Hash of the data (could be either a message hash or transaction hash)
+     * @param data That should be signed (this is passed to an external validator contract)
+     * @param signatures Signature data that should be verified. Can be ECDSA signature, contract signature (EIP-1271) or approved hash.
+     * @param gnosisSafe gnosis safe interface
+     */
+    function checkNSignatures(
+        bytes32 dataHash,
+        bytes memory data,
+        bytes memory signatures,
+        GnosisSafe gnosisSafe
+    ) public view {
+        uint256 requiredSignatures = gnosisSafe.getThreshold();
+        // Check that the provided signature data is not too short
+        require(signatures.length >= requiredSignatures.mul(65), "GS020");
+        // There cannot be an owner with address 0.
+        address lastOwner = address(0);
+        address currentOwner;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 i;
+        for (i = 0; i < requiredSignatures; i++) {
+            (v, r, s) = signatureSplit(signatures, i);
+            if (v == 0) {
+                // If v is 0 then it is a contract signature
+                // When handling contract signatures the address of the contract is encoded into r
+                currentOwner = address(uint160(uint256(r)));
+
+                // Check that signature data pointer (s) is not pointing inside the static part of the signatures bytes
+                // This check is not completely accurate, since it is possible that more signatures than the threshold are send.
+                // Here we only check that the pointer is not pointing inside the part that is being processed
+                require(uint256(s) >= requiredSignatures.mul(65), "GS021");
+
+                // Check that signature data pointer (s) is in bounds (points to the length of data -> 32 bytes)
+                require(uint256(s).add(32) <= signatures.length, "GS022");
+
+                // Check if the contract signature is in bounds: start of data is s + 32 and end is start + signature length
+                uint256 contractSignatureLen;
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    contractSignatureLen := mload(add(add(signatures, s), 0x20))
+                }
+                require(uint256(s).add(32).add(contractSignatureLen) <= signatures.length, "GS023");
+
+                // Check signature
+                bytes memory contractSignature;
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    // The signature data for contract signatures is appended to the concatenated signatures and the offset is stored in s
+                    contractSignature := add(add(signatures, s), 0x20)
+                }
+                require(ISignatureValidator(currentOwner).isValidSignature(data, contractSignature) == EIP1271_MAGIC_VALUE, "GS024");
+            // }
+            // TODO: Identify this usecase
+            // else if (v == 1) {
+            //     // If v is 1 then it is an approved hash
+            //     // When handling approved hashes the address of the approver is encoded into r
+            //     currentOwner = address(uint160(uint256(r)));
+            //     // Hashes are automatically approved by the sender of the message or when they have been pre-approved via a separate transaction
+            //     require(msg.sender == currentOwner || approvedHashes[currentOwner][dataHash] != 0, "GS025");
+            // 
+            } else if (v > 30) {
+                // If v > 30 then default va (27,28) has been adjusted for eth_sign flow
+                // To support eth_sign and similar we adjust v and hash the messageHash with the Ethereum message prefix before applying ecrecover
+                currentOwner = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", dataHash)), v - 4, r, s);
+            } else {
+                // Default is the ecrecover flow with the provided data hash
+                // Use ecrecover with the messageHash for EOA signatures
+                currentOwner = ecrecover(dataHash, v, r, s);
+            }
+            require(currentOwner > lastOwner && currentOwner != SENTINEL_OWNERS, "GS026");
+            // TODO change this logic, not optimized: Check current owner is part of the owners of the safe
+            require(isSafeOwner(gnosisSafe, currentOwner) != false, "GS026");
+            lastOwner = currentOwner;
+        }
+    }
+
+    function isSafeOwner(GnosisSafe gnosisSafe, address signer) private view returns (bool) {
+        address[] memory safeOwners = gnosisSafe.getOwners();
+        for (uint256 i = 0; i < safeOwners.length; i++) {
+            if (safeOwners[i] == signer) {
+                return true;
+            }
+        }
+        return false;
     }
 }
