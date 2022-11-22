27729624ad42094f1d238e660f6d4e9b550bcaab
Jose Ramirez <ramirez.jos@gmail.com>
2022-11-22 13:27:06 +0100

    refactoring: separate deny helpers only tests, move mocked contracts to folder

--- test/mocks/DenyHelperMockedContract.t.sol
+++ test/mocks/DenyHelperMockedContract.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {DenyHelper} from "../src/DenyHelper.sol";
+import {DenyHelper} from "../../src/DenyHelper.sol";
 
 contract DenyHelperMockedContract is DenyHelper {
     address private mockAddress = address(0x321);
